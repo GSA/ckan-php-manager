0ae8b13867a9577230fc6b8318dcbee7ad934c9d
alex-perfilov-reisys <alexandr.perfilov@reisystems.com>
2014-07-02 17:37:10 -0400

    Finding socrata dms non-interactive datasets for redirects

--- cli/find_socrata_txt_pairs.php
+++ cli/find_socrata_txt_pairs.php
@@ -13,7 +13,7 @@ $limit = isset($argv[2]) ? intval($argv[2]) : 1;
 /**
  * Create results dir for logs
  */
-$results_dir = RESULTS_DIR . date('/Ymd-His') . '_LIST_REDIRECT_DATASETS_' . $start ? : '';
+$results_dir = RESULTS_DIR . date('/Ymd-His') . '_SOCRATA_PAIRS';
 mkdir($results_dir);
 
 /**
@@ -32,7 +32,24 @@ $Importer = new \CKAN\Manager\CkanManager(CKAN_API_URL, CKAN_API_KEY);
  */
 //$Importer = new \CKAN\Manager\CkanManager(CKAN_DEV_API_URL, CKAN_DEV_API_KEY);
 
-define('ERROR_REPORTING', E_ALL);
+/**
+ *
+ */
+define('ERROR_REPORTING', E_ALL & ~E_NOTICE);
+
+//  https://explore.data.gov/api/views/bxfh-jivs.json
+/**
+ *
+ */
+define('SOCRATA_URL', 'https://explore.data.gov/api/views/');
+
+if (!is_readable($socrata_file_path = DATA_DIR . '/socrata.txt')) {
+    die($socrata_file_path . ' not readable');
+}
+
+$socrata_list = file_get_contents($socrata_file_path);
+$socrata_list = preg_replace('/[\\r\\n]+/', "\n", $socrata_list);
+$socrata_list = explode("\n", $socrata_list);
 
 /**
  * Get organization terms, including all children, as Array
@@ -40,7 +57,7 @@ define('ERROR_REPORTING', E_ALL);
 $OrgList    = new \CKAN\Core\OrganizationList(AGENCIES_LIST_URL);
 $termsArray = $OrgList->getTreeArray();
 
-$Importer->get_redirect_list($termsArray, $results_dir, $start, $limit);
+$Importer->get_socrata_pairs($socrata_list, $results_dir);
 
 // show running time on finish
 timer();
