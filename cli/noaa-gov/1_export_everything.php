3c5556222c57e902ae01d60b3384123db89891ed
Alex Perfilov <alexandr.perfilov@reisystems.com>
2015-09-14 17:28:02 -0400

    noaa-gov export :gem:

---
+++ cli/noaa-gov/1_export_everything.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CKAN\Manager;
+
+
+use CKAN\OrganizationList;
+
+/**
+ * http://idm.data.gov/fed_agency.json
+ */
+define('ORGANIZATION_TO_EXPORT', 'Nuclear Regulatory Commission');
+
+require_once dirname(dirname(__DIR__)) . '/inc/common.php';
+
+/**
+ * Get organization terms, including all children, as Array
+ */
+$OrgList = new OrganizationList(AGENCIES_LIST_URL);
+$termsArray = $OrgList->getTreeArrayFor(ORGANIZATION_TO_EXPORT);
+
+/**
+ * sometimes there is no parent term (ex. Department of Labor)
+ */
+if (!defined('PARENT_TERM')) {
+    define('PARENT_TERM', '_');
+}
+
+/**
+ * Create results dir for logs and json results
+ */
+$results_dir = RESULTS_DIR . date('/Ymd-His') . '_EXPORT_' . PARENT_TERM;
+mkdir($results_dir);
+
+/**
+ * Search for packages by terms found
+ */
+
+/**
+ * Production
+ */
+$CkanManager = new CkanManager(CKAN_API_URL);
+//$CkanManager = new CkanManager(CKAN_QA_API_URL);
+//$CkanManager = new CkanManager(INVENTORY_CKAN_PROD_API_URL, INVENTORY_CKAN_PROD_API_KEY);
+
+/**
+ * Staging
+ */
+//$CkanManager = new CkanManager(CKAN_STAGING_API_URL);
+
+$CkanManager->resultsDir = $results_dir;
+
+$CkanManager->exportPackagesByOrgTerms($termsArray);
+
+// show running time on finish
+timer();
